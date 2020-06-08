ac4eb4938624945fa0a9c66394bcddde7f8e89f5
solka13Lenio <solomialenio13@gmail.com>
2020-06-08 23:38:39 +0300

    my profile and log put

--- modules/app.py
+++ modules/app.py
@@ -20,6 +20,34 @@ def connect_to_db():
     return conn
 
 
+def get_alcoholic_stats():
+    """
+    Returns dict of lists of alcoholics of certain categories:
+    inspector's favourite,
+    inspector's disfavourite,
+    drinking master,
+    quickest legs,
+    most friendly.
+    :return: dict
+    """
+    alcoholics = dict()
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute(f"""select * from alcoholic;""")
+    alcoholics['alcoholics'] = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # TODO: run queries to get corresponding alcoholics for each type
+    alcoholics['friendly'] = alcoholics['alcoholics']
+    alcoholics['quick'] = alcoholics['alcoholics']
+    alcoholics['master'] = alcoholics['alcoholics']
+    alcoholics['favourite'] = alcoholics['alcoholics']
+    alcoholics['disfavourite'] = alcoholics['alcoholics']
+
+    return alcoholics
+
 @app.route('/', methods=["GET", "POST"])
 def login_page():
     error = ""
@@ -89,22 +117,22 @@ def home():
 
         context['errors'] = errors
 
-    context['user_data'] = json.loads(request.args['user_data'])
+    try:
+        context['user_data'] = json.loads(request.args['user_data'])
+    except KeyError:
+        context['user_data'] = {
+            'user_id': request.args['user_id'],
+            'user_type': request.args['user_type']
+        }
 
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute(f"""select * from alcoholic;""")
-    context['alcoholics'] = cur.fetchall()
-    conn.commit()
-    cur.close()
-    conn.close()
+    alcoholics = get_alcoholic_stats()
 
-    # TODO: run queries to get corresponding alcoholics for each type
-    context['friendly'] = context['alcoholics']
-    context['quick'] = context['alcoholics']
-    context['master'] = context['alcoholics']
-    context['favourite'] = context['alcoholics']
-    context['disfavourite'] = context['alcoholics']
+    context['alcoholics'] = alcoholics['alcoholics']
+    context['friendly'] = alcoholics['alcoholics']
+    context['quick'] = alcoholics['alcoholics']
+    context['master'] = alcoholics['alcoholics']
+    context['favourite'] = alcoholics['alcoholics']
+    context['disfavourite'] = alcoholics['alcoholics']
 
     return render_template('home.html', context=context)
 
@@ -122,12 +150,59 @@ def my_profile():
     cur = conn.cursor()
     cur.execute(f"""select * from alcoholic where alcoholic_id = {user_id};""")
     response = cur.fetchall()
-    conn.commit()
-    cur.close()
-    conn.close()
 
     context = dict()
-    context['user_data'] = response[0]
+    context['user'] = response[0]
+    context['alcoholics'] = get_alcoholic_stats()
+
+    if request.method == "POST":
+        data = dict(request.form)
+        errors = list()
+        if data['action'] == "escape":
+            cur.execute(f"""select enclosed from alcoholic where alcoholic_id ={ data['user_id'] };""")
+            response = cur.fetchall()
+            conn.commit()
+            if not response[0][0]:
+                errors.append("you cannot escape sober-up - you're not there! enjoy your freedom <3")
+            else:
+                cur.execute(f"""select current_date - migration_date from migrations where alcoholic_id = { data['user_id'] } 
+                                and bed_to is not NULL order by migration_date desc limit 1;""")
+                response = cur.fetchall()
+                if response[0][0] < 2:
+                    error.append("you cannot escape sober-up - you are still drunk!")
+                else:
+                    cur.execute(f"""update alcoholic set enclosed = False where alcoholic_id = { data['user_id'] };""")
+                    conn.commit()
+
+                    cur.execute(f"""select bed_id from alcoholic_bed where alcoholic_id={ data['user_id'] } and date_to is NULL;""")
+                    bed_id = cur.fetchall()[0][0]
+
+                    cur.execute(f"""insert into escape(alcoholic_id, escape_date, bed_id) values ({ data['user_id'] }, current_date, { bed_id });""")
+                    conn.commit()
+        else:  # action == "gain consciousness"
+            cur.execute(f"""select conscious from alcoholic where alcoholic_id ={data['user_id']};""")
+            response = cur.fetchall()
+            conn.commit()
+            if response[0][0]:
+                errors.append("you cannot gain consciousness - you're all right!")
+            else:
+                cur.execute(
+                    f"""select current_date - date_from from faints where alcoholic_id = { data['user_id']} 
+                                            and date_to is NULL order by date_from desc limit 1;""")
+                response = cur.fetchall()
+                if response and response[0] < 3:
+                    error.append("you cannot gain consciousness - you're still ill!")
+                else:
+                    cur.execute(f"""update alcoholic set conscious = True where alcoholic_id = {data['user_id'] };""")
+                    conn.commit()
+
+                    cur.execute(f"""update faints set date_to = current_date where alcoholic_id={ data['user_id'] } and date_to is NULL;""")
+                    conn.commit()
+        cur.close()
+        conn.close()
+
+        context['errors'] = errors
+
     return render_template('my_profile.html', context=context)
 
 
